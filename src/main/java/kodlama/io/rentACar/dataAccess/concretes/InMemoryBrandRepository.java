9283327a50986c8698e47586859c3392dd1ea913
kubilaybee <kubilaybee@gmail.com>
2022-11-03 02:36:26 +0300

    spring-2 completed -lombok,postgresql

--- src/main/java/kodlama/io/rentACar/dataAccess/concretes/InMemoryBrandRepository.java
+++ src/main/java/kodlama/io/rentACar/dataAccess/concretes/InMemoryBrandRepository.java
@@ -1,27 +1,27 @@
-package kodlama.io.rentACar.dataAccess.concretes;
-
-import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
-import kodlama.io.rentACar.entities.concretes.Brand;
-import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Repository // -> that class is dataAccess Object
-public class InMemoryBrandRepository implements BrandRepository {
-    List<Brand> brands;
-
-    public InMemoryBrandRepository() {
-        brands = new ArrayList<Brand>();
-        brands.add(new Brand(0,"Audi"));
-        brands.add(new Brand(1,"BMW"));
-        brands.add(new Brand(2,"Bugatti"));
-        brands.add(new Brand(3,"Fiat"));
-        brands.add(new Brand(4,"Mercedes"));
-    }
-
-    @Override
-    public List<Brand> getAll() {
-        return brands;
-    }
-}
+//package kodlama.io.rentACar.dataAccess.concretes;
+//
+//import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+//import kodlama.io.rentACar.entities.concretes.Brand;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Repository // -> that class is dataAccess Object
+//public class InMemoryBrandRepository implements BrandRepository {
+//    List<Brand> brands;
+//
+//    public InMemoryBrandRepository() {
+//        brands = new ArrayList<Brand>();
+//        brands.add(new Brand(0,"Audi"));
+//        brands.add(new Brand(1,"BMW"));
+//        brands.add(new Brand(2,"Bugatti"));
+//        brands.add(new Brand(3,"Fiat"));
+//        brands.add(new Brand(4,"Mercedes"));
+//    }
+//
+//    @Override
+//    public List<Brand> getAll() {
+//        return brands;
+//    }
+//}
--- src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,31 +1,22 @@
 package kodlama.io.rentACar.entities.concretes;
 
+import javax.persistence.*;
+
+import lombok.*;
+
+
+@Table(name="brands")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "name")
     private String name;
-
-    // constructor
-    public Brand() {
-    }
-
-    public Brand(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
